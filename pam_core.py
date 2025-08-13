ae3478d43101af95bf897be8aa4d36ecec1bbba1
Benjeoron <benjeoron@gmail.com>
2025-08-13 01:34:58 -0700

    Implementing add()
    Addition of passwords to vault is seemingly tricky

--- pam_core.py
+++ pam_core.py
@@ -2,55 +2,50 @@ import argon2
 import os
 import sqlite3
 import Crypto.Cipher.AES as AES
+import secrets
 
 def pam_verify(pwd : str) -> tuple[bool, str]:
     db = sqlite3.connect("./.pam/pam.db")
     cs = db.cursor()
-    vals = cs.execute("SELECT hash, salt from keys WHERE is_master = 1").fetchall()
-    try:
-        ph = argon2.PasswordHasher
-        ph.verify(vals[0][0], pwd)
-        return (True, ph.hash( password=pwd,salt=vals[0][1] ))
-    except:
-        print("Incorrect master password.")
-        return (False, "")
+    vals : tuple[tuple[str, str]]= cs.execute("SELECT hash, salt from keys WHERE is_master = 1").fetchall()
+    # try:
+    ph = argon2.PasswordHasher()
+    ph.verify(hash=vals[0][0], password=pwd)
+    return (True, ph.hash(password=pwd, salt=vals[0][1].encode()))
+    # except:
+    #     return (False, "")
 
-def _check_cwd() -> tuple[bool, bool]:
+def check_cwd() -> tuple[bool, bool]:
     return (os.path.exists("./.pam"), os.path.exists("./.pam/pam.db"))
 
-def pam_init(pwd : str) -> None:
-    check = _check_cwd()
-    if not check[0]:
-        os.mkdir("./.pam")
+def pam_init(pwd : str, check : tuple[bool, bool]) -> None:
     if check[1]:
-        while True:
-            answer = input("Warning, there is already an instance of pam set up here. Do you wish to " \
-            "overwrite the instance, deleting all of the stored passwords? [y/n] ")
-            if answer.lower() == "y" or answer.lower() == "yes":
-                os.remove("./.pam/pam.db")
-                break
-            elif answer.lower() == "n" or answer.lower() == "no":
-                return
+        os.remove("./.pam/pam.db")
     db = sqlite3.connect("./.pam/pam.db")
     curs = db.cursor()
-    salt = os.urandom(32)
+    salt = secrets.token_bytes(32)
     master_hash = argon2.PasswordHasher().hash(password=pwd, salt=salt)
+
     curs.execute("CREATE TABLE keys (service TEXT, username TEXT, hash TEXT, salt TEXT, is_master INTEGER)")
-    curs.execute("INSERT INTO keys VALUES (?, ?, ?, ?, ?)", ("pam", "MASTER", master_hash, os.urandom(32).hex(), 1))
+    curs.execute("INSERT INTO keys VALUES (?, ?, ?, ?, ?)", ("pam", "MASTER", master_hash, secrets.token_bytes(32).hex(), 1))
     curs.execute("CREATE TABLE passwords (service TEXT, username TEXT, ciphertext TEXT)")
     db.commit()
     db.close()
 
-def pam_add(service : str, username : str, pwd : str) -> None:
-    if not all(check for check in _check_cwd()):
+def pam_add(service : str, username : str, pwd : str, key : str) -> None:
+    if not all(check for check in check_cwd()):
         print("Please initialize pam in this directory first by running \"py pam init\"")
         return
+
     db = sqlite3.connect("./.pam/pam.db")
     cs = db.cursor()
-    salt = os.urandom(32)
-    hash = argon2.PasswordHasher()
-    cs.execute("INSERT")
+    salt = secrets.token_bytes(32)
+    dek = argon2.PasswordHasher().hash(password=key, salt=salt)
+    cs.execute("INSERT INTO keys VALUES (?, ?, ?, ?, ?)", (service, username, dek, salt, 0))
 
-    AES.new(mode=AES.MODE_GCM)
+    nonce = secrets.token_bytes(16)
+    aes = AES.new(key=dek, mode=AES.MODE_GCM, nonce=nonce)
+    enc = aes.encrypt(pwd)
+    cs.execute("INSERT INTO passwords VALUES (?, ?, ?)", (service, username, enc))
 
 
