81ca160912ba2c1e21c74a35120a8b0c65af63e7
Benjeoron <50589845+Benjeoron@users.noreply.github.com>
2025-09-04 16:32:50 -0700

    Update pam_core.py

--- pam_core.py
+++ pam_core.py
@@ -3,17 +3,18 @@ import os
 import sqlite3
 import Crypto.Cipher.AES as AES
 import secrets
+import base64
 
 def pam_verify(pwd : str) -> tuple[bool, str]:
     db = sqlite3.connect("./.pam/pam.db")
     cs = db.cursor()
-    vals : tuple[tuple[str, str]]= cs.execute("SELECT hash, salt from keys WHERE is_master = 1").fetchall()
-    # try:
-    ph = argon2.PasswordHasher()
-    ph.verify(hash=vals[0][0], password=pwd)
-    return (True, ph.hash(password=pwd, salt=vals[0][1].encode()))
-    # except:
-    #     return (False, "")
+    vals : list[tuple[str, str]]= cs.execute("SELECT hash, salt from keys WHERE is_master = 1").fetchall()
+    try:
+        ph = argon2.PasswordHasher()
+        ph.verify(hash=vals[0][0], password=pwd)
+        return (True, ph.hash(password=pwd, salt=vals[0][1].encode()))
+    except:
+        return (False, "")
 
 def check_cwd() -> tuple[bool, bool]:
     return (os.path.exists("./.pam"), os.path.exists("./.pam/pam.db"))
@@ -40,12 +41,14 @@ def pam_add(service : str, username : str, pwd : str, key : str) -> None:
     db = sqlite3.connect("./.pam/pam.db")
     cs = db.cursor()
     salt = secrets.token_bytes(32)
-    dek = argon2.PasswordHasher().hash(password=key, salt=salt)
-    cs.execute("INSERT INTO keys VALUES (?, ?, ?, ?, ?)", (service, username, dek, salt, 0))
-
+    full_dek = argon2.PasswordHasher(hash_len=16).hash(password=key, salt=salt)
+    # cs.execute("INSERT INTO keys VALUES (?, ?, ?, ?, ?)", (service, username, dek, salt, 0))
+    dek = base64.b64decode(full_dek.split("$")[5]+"==").hex()
     nonce = secrets.token_bytes(16)
-    aes = AES.new(key=dek, mode=AES.MODE_GCM, nonce=nonce)
-    enc = aes.encrypt(pwd)
-    cs.execute("INSERT INTO passwords VALUES (?, ?, ?)", (service, username, enc))
+    aes = AES.new(key=dek.encode(), mode=AES.MODE_GCM, nonce=nonce)
+    enc = aes.encrypt(plaintext=pwd.encode())
+    print(enc)
+    # cs.execute("INSERT INTO passwords VALUES (?, ?, ?)", (service, username, enc))
+    # db.commit()
 
 
